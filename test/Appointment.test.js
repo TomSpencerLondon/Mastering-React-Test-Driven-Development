c033fb9a67487a4dde621d1b24959b6ec0048025
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-04-11 22:53:34 +0100

    passing test for appointment

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,15 @@
+import {Appointment } from '../src/Appointment';
+import ReactDOM from 'react-dom';
+import React from "react";
+
 describe('Appointment', () => {
     it('renders the customer first name', () => {
+        const customer = { firstName: 'Ashley' };
+        const component = <Appointment customer={customer} />;
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+        ReactDOM.render(component, container);
+
         expect(document.body.textContent).toMatch('Ashley');
     })
 });
